1720ac3d17b4dd1d04d2ffd5c16152a23728336a
TorturedDude <Artem110902@yandex.ru>
2022-07-22 16:46:11 -0700

    realize classes lorry and sport car

--- src/com/company/vehicles/Lorry.java
+++ src/com/company/vehicles/Lorry.java
@@ -1,4 +1,39 @@
 package com.company.vehicles;
 
-public class Lorry {
+import com.company.details.Engine;
+import com.company.professions.Driver;
+
+public class Lorry extends Car{
+
+    private int carrying;
+
+    public double getCarrying(){
+        return this.carrying;
+    }
+
+    public void setCarrying(int carrying){
+        if (carrying <= 0){
+            System.out.println("Вы ввели некорректную грузоподъёмность");
+        }
+        else {
+            this.carrying = carrying;
+        }
+    }
+
+    public Lorry(String carClass, double weight, String brand, Engine engine, Driver driver, int carrying) {
+        super(carClass, weight, brand, engine, driver);
+
+        if (carrying <= 0){
+            System.out.println("Вы ввели некорректную грузоподъёмность");
+        }
+        else {
+            this.carrying = carrying;
+        }
+
+    }
+
+    @Override
+    public String toString(){
+        return String.format("Автомобиль класса %s такого брэнда, как %s с весом %f, c грузоподъёмностью %d ,с " + this.engine + ", с водителем " + this.driver, this.carClass, this.brand, this.weight, this.carrying);
+    }
 }
--- src/com/company/vehicles/SportCar.java
+++ src/com/company/vehicles/SportCar.java
@@ -1,4 +1,39 @@
 package com.company.vehicles;
 
-public class SportCar {
+import com.company.details.Engine;
+import com.company.professions.Driver;
+
+public class SportCar extends Car{
+
+    private double maxSpeed;
+
+    public double getMaxSpeed(){
+        return this.maxSpeed;
+    }
+
+    public void setMaxSpeed(double maxSpeed){
+        if (maxSpeed <= 0){
+            System.out.println("Вы ввели некорректную предельную скорость");
+        }
+        else {
+            this.maxSpeed = maxSpeed;
+        }
+    }
+
+    public SportCar(String carClass, double weight, String brand, Engine engine, Driver driver, double maxSpeed) {
+        super(carClass, weight, brand, engine, driver);
+
+        if (maxSpeed <= 0){
+            System.out.println("Вы ввели некорректную предельную скорость");
+        }
+        else {
+            this.maxSpeed = maxSpeed;
+        }
+
+    }
+
+    public String toString(){
+        return String.format("Автомобиль класса %s такого брэнда, как %s с весом %f, c предельной скоростью %f ,с " + this.engine + ", с водителем " + this.driver, this.carClass, this.brand, this.weight, this.maxSpeed);
+    }
+
 }
