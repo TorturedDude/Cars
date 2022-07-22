c1fde9aa8d070b0c246977aa5ba426a0f0e204c4
TorturedDude <Artem110902@yandex.ru>
2022-07-22 16:02:23 -0700

    realize class person and driver

--- src/com/company/professions/Driver.java
+++ src/com/company/professions/Driver.java
@@ -1,4 +1,37 @@
 package com.company.professions;
 
-public class Driver {
+public class Driver extends Person{
+
+    private double experience;
+
+    public double getExperience(){
+        return this.experience;
+    }
+
+    public void setExperience(double experience){
+        if (experience < 0){
+            System.out.println("Вы ввели некорректный стаж вождения");
+        }
+        else {
+            this.experience = experience;
+        }
+    }
+
+    public Driver(String inicials, int age, double experience) {
+        super(inicials, age);
+
+        if (experience < 0){
+            System.out.println("Вы ввели некорректный стаж вождения");
+        }
+        else {
+            this.experience = experience;
+        }
+
+    }
+
+    @Override
+    public String toString(){
+        return String.format("%s возрастом %d годов имеет стаж %f", this.inicials, this.age, this.experience);
+    }
+
 }
--- src/com/company/professions/Person.java
+++ src/com/company/professions/Person.java
@@ -1,4 +1,46 @@
 package com.company.professions;
 
 public class Person {
+
+    protected String inicials;
+    protected int age;
+
+    public String getInicials(){
+        return this.inicials;
+    }
+
+    public int getAge(){
+        return this.age;
+    }
+
+    public void setInicials(String inicials){
+        this.inicials = inicials;
+    }
+
+    public void setAge(int age){
+        if (age < 18){
+            System.out.println("Вы ввели некорректный возраст");
+        }
+        else {
+            this.age = age;
+        }
+    }
+
+    public Person(String inicials, int age){
+
+        this.inicials = inicials;
+
+        if (age < 18){
+            System.out.println("Вы ввели некорректный возраст");
+        }
+        else {
+            this.age = age;
+        }
+
+    }
+
+    public String toString(){
+        return String.format("%s возрастом %d годов", this.inicials, this.age);
+    }
+
 }
