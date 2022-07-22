f39140948ef7851ac24bb9130bed7af6d279edcc
TorturedDude <Artem110902@yandex.ru>
2022-07-22 16:09:07 -0700

    realize class engine

--- src/com/company/details/Engine.java
+++ src/com/company/details/Engine.java
@@ -1,4 +1,46 @@
 package com.company.details;
 
 public class Engine {
+
+    private String company;
+    private int power;
+
+    public String getCompany(){
+        return this.company;
+    }
+
+    public int getPower(){
+        return this.power;
+    }
+
+    public void setCompany(String company){
+        this.company = company;
+    }
+
+    public void setPower(int power){
+        if (power <= 0){
+            System.out.println("Вы ввели некорректную мощность двигателя");
+        }
+        else {
+            this.power = power;
+        }
+    }
+
+    public Engine(String company, int power){
+
+        this.company = company;
+
+        if (power <= 0){
+            System.out.println("Вы ввели некорректную мощность двигателя");
+        }
+        else {
+            this.power = power;
+        }
+
+    }
+
+    public String toString(){
+        return String.format("Двигатель комании : '%s', с мощьностью %d", this.company, this.power);
+    }
+
 }
